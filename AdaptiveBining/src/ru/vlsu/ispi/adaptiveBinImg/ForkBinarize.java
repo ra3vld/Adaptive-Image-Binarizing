9c3601c4d51c73030a09e035d7ea6a1ba26b30a9
1 <1@asuspc>
2014-11-24 22:53:44 +0400

    Initial commit

--- AdaptiveBining/src/ru/vlsu/ispi/adaptiveBinImg/ForkBinarize.java
+++ AdaptiveBining/src/ru/vlsu/ispi/adaptiveBinImg/ForkBinarize.java
@@ -19,12 +19,19 @@ public class ForkBinarize extends RecursiveAction {
     int start;
     int length;
 
-    boolean adaptive = false;
     int threshold;
     int windowSize = 0;
     int brightTreshold = 128;
-    int[][] raster;
 
+    /**
+     *
+     * @param source - source image
+     * @param destination - result image
+     * @param start - start line for task
+     * @param length - number of line for task
+     * @param threshold - maximal length
+     * @param windowSize - side of square for averaging bright
+     */
     public ForkBinarize(BufferedImage source, BufferedImage destination, int start, int length, int threshold, int windowSize) {
         this.source = source;
         this.destination = destination;
@@ -39,31 +46,37 @@ public class ForkBinarize extends RecursiveAction {
         int bright;
         int white = 0x00ffffff;
         int black = 0;
+        //bypassing pixels in this task
         for (int y = start; y < start + length; y++) {
             for (int x = 0; x < source.getWidth(); x++) {
                 int color;
-                bright = getAvgBright(source.getRGB(x, y));
+                
+                bright = getAvgBrightOfPixel(source.getRGB(x, y));
 
                 avgBright = getWindowAvg(source, x, y, windowSize);
-
+                //optimization for noncontrast image
                 if (bright - avgBright < -25 || bright - avgBright > 15) {
                     color = bright >= avgBright ? white : black;
                 } else {
                     color = bright > brightTreshold ? white : black;
-
                 }
                 destination.setRGB(x, y, color);
             }
         }
     }
-
-    private static int getAvgBright(int rgba) {
+    //get average bright of pixel
+    private int getAvgBrightOfPixel(int rgba) {
         int sumBrightB = rgba & 0x000000ff;
         int sumBrightG = (rgba & 0x0000ff00) >> 8;
         int sumBrightR = (rgba & 0x00ff0000) >> 16;
         return (sumBrightB + sumBrightG + sumBrightR) / 3;
     }
 
+    //processing avarege bright of window square with center with current pixel
+    //for border pixel all aboard pixel doesn't calculate
+    //(sic!) getRGB so slow; with simple int[][] instead Image up to 10 times faster
+    //(sic!) every pixel calculating new window, its very slowly
+    //
     private int getWindowAvg(BufferedImage img, int centerX, int centerY, int windowsSize) {
         int halfWin = windowsSize / 2;
         int minX = centerX - halfWin;
@@ -76,23 +89,21 @@ public class ForkBinarize extends RecursiveAction {
         int maxY = centerY + halfWin;
         maxY = maxY > img.getHeight() ? img.getHeight() : maxY;
 
-        int sumBrightR = 0, sumBrightG = 0, sumBrightB = 0;
+        int sumBright = 0;
         int pixel;
         for (int x = minX; x < maxX; x++) {
             for (int y = minY; y < maxY; y++) {
                 pixel = img.getRGB(x, y);
-                sumBrightB += pixel & 0x000000ff;
-                sumBrightG += (pixel & 0x0000ff00) >> 8;
-                sumBrightR += (pixel & 0x00ff0000) >> 16;
+                sumBright += getAvgBrightOfPixel(pixel);
             }
         }
         int lengthX = maxX - minX;
         int lengthY = maxY - minY;
-        int sumBright = (sumBrightB + sumBrightG + sumBrightR) / 3;
         int avgBright = sumBright / (lengthX * lengthY);
         return avgBright;
     }
 
+    //task is divided by lines
     @Override
     protected void compute() {
         if (length < threshold) {//compute directly
